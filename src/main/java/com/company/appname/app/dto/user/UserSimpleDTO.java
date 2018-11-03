5e187532c16b27c8d139e0fdef49261da0aacbde
vakmancordero <vakmancordero@gmail.com>
2018-11-02 22:57:53 -0600

    Adding ApiModel to UserSimpleDTO...

--- src/main/java/com/company/appname/app/dto/user/UserSimpleDTO.java
+++ src/main/java/com/company/appname/app/dto/user/UserSimpleDTO.java
@@ -1,9 +1,11 @@
 package com.company.appname.app.dto.user;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description="Simple detail about the user.")
 public class UserSimpleDTO {
 
     @ApiModelProperty(notes = "User identifier")
